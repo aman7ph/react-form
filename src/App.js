8e49c01c2408d19ec6d3a7559321a3c527641d10
amanph <122424467+amanph@users.noreply.github.com>
2023-07-29 06:46:21 +0300

    state hook declored

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import './style.css';
 import FormInput from './componet/FormInput';
+
 export default function App() {
+ const [values,setValues]=React.useState({
+    username="",
+    email="",
+    birthay="",
+    password="",
+    confirmPassword="",
+  })
+
+ const Inputs=[{
+   id:1,
+   name:"username",
+   type:"text",
+   placeholder="username",
+   lable="username"
+
+ },
+ {
+  id:2,
+  name:"email",
+  type:"email",
+  placeholder="email",
+  lable="email"
+
+},
+{
+  id:3,
+  name:"birthay",
+  type:"text",
+  placeholder="Birthay",
+  lable="Birthay"
+
+},
+{
+  id:4,
+  name:"password",
+  type:"password",
+  placeholder="password",
+  lable="password"
+
+},
+{
+  id:5,
+  name:"confirmPassword",
+  type:"password",
+  placeholder="confirm Password",
+  lable="confirm Password"
+
+},]
+
+  const handleSubmit=(e)=>{
+       e.preventDefault()
+
+  }
+
   return (
     <div className="app">
-      <form action="">
-        <FormInput placeholder="username" />
-        <FormInput placeholder="Email" />
-        <FormInput placeholder="Full Name" />
-        <FormInput placeholder="sth" />
+      <form onSubmit={handleSubmit}>
+        <FormInput name="username" placeholder="username" />
+        <FormInput name="email" placeholder="Email" />
+        <FormInput name="full name" placeholder="Full Name" />
+        <FormInput name="sth" placeholder="sth" />
+        <button>Submit</button>
       </form>
     </div>
   );
--- src/componet/FormInput.js
+++ src/componet/FormInput.js
@@ -3,8 +3,7 @@ import './forminput.css';
 const FormInput = (props) => {
   return (
     <div className="formInput">
-      <label>Username</label>
-      <input placeholder ={props.placeholder } />
+      <input name={props.name} placeholder ={props.placeholder } />
     </div>
   );
 };
