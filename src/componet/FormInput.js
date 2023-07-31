275a6ba04d7fa25b72a4caf542156a3ca8b0d5da
amanph <122424467+amanph@users.noreply.github.com>
2023-07-31 13:53:38 +0300

    i18next

--- src/componet/FormInput.js
+++ src/componet/FormInput.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './forminput.css';
 const FormInput = (props) => {
-  const { id, lable, onChange, ...inputProps } = props;
+  const { id, lable, errormessage, onChange, ...inputProps } = props;
   return (
     <div className="formInput">
       <label htmlFor="">{lable}</label>
       <input {...inputProps} onChange={onChange} />
+      <span>{errormessage}</span>
     </div>
   );
 };
