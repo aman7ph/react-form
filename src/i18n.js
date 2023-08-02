2e6ed8f95be0a7bbd39f8ad2ff2a2856a5367aaa
amanph <122424467+amanph@users.noreply.github.com>
2023-08-02 06:37:51 +0300

    working with i18next

--- src/i18n.js
+++ src/i18n.js
@@ -1,30 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from "i18next-http-backend"
 
 i18n
-.use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(Backend)
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
     fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+    debug: true,
+    returnObjects: true,
+    whitelist: ['en', 'amh'], // add language codes that you want.
+    detection: {
+      order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+      checkWhiteList: true,
     },
-    resources: {
-      en: {
-        translation: {
-          // here we will place our translations...
-        }
-      }
-    }
   });
-
+i18n.changeLanguage('amh');
 export default i18n;
