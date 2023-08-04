ecaaabd5804b58bb6f11316943623e898a5cf7d2
amanph <122424467+amanph@users.noreply.github.com>
2023-08-04 15:14:52 +0300

    error message

--- src/componet/FormInput.js
+++ src/componet/FormInput.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './forminput.css';
 const FormInput = (props) => {
-  const { id, lable, errormessage, onChange, ...inputProps } = props;
+  const { id, lable, errorMessage, onChange, ...inputProps } = props;
   return (
     <div className="formInput">
       <label htmlFor="">{lable}</label>
       <input {...inputProps} onChange={onChange} />
-      <span>{errormessage}</span>
+      <span>{errorMessage}</span>
     </div>
   );
 };
